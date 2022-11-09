bca408a2eb9bb70fa1ee3f359dea9b522ab1676c
kartikkunwar <kunwarkartik@gmail.com>
2022-11-09 16:05:10 +0530

    initlal work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navbar} from "./components/navbar"
+import { Footer } from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <Footer/>
     </div>
   );
 }
